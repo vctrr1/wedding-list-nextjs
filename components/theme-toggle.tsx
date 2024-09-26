8abf68aa06aa8e2ce06789d4dcaf445c1a18e949
Victor Emanoel <victoremaanoel@outlook.com>
2024-09-26 20:43:27 -0300

    fix: correct toggle theme render after client

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+import {useState, useEffect} from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
@@ -9,16 +9,28 @@ import { Button } from "@/components/ui/button"
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
+  //essa variavel serve para controlar quando o componente foi montado no cliente. 
+  //Isso evita que o componente seja renderizado no servidor, onde o tema ainda não está disponível.
+  const [mounted, setMounted] = useState(false)
+  
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light")
   }
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
       {theme === "light" ? (
         <Moon className="h-[1.2rem] w-[1.2rem]" />
-    ) : (
-          <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
