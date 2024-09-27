0037e9b105650cbc831d5e6cf7b25171299b824d
Victor Emanoel <victoremaanoel@outlook.com>
2024-09-27 01:26:32 -0300

    chore: add card ui and update navbar text and install authJS and prisma

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
     <div className="flex flex-row border-b items-center justify-between p-3">
         <div className="hidden sm:flex w-[88px]"></div>
-        <h1 className="text-xl">Lista de Enxoval</h1>
+        <h1 className="text-2xl">Lista de Enxoval</h1>
         <div className="gap-2 flex">
           <ThemeToggle/>     
           <Button size="icon" variant="ghost">
