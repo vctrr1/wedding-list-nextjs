d88fdd49a717d12194cff3e9115a20adcd5b872d
Victor Emanoel <victoremaanoel@outlook.com>
2024-09-27 01:27:50 -0300

    feat: created input product

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { PlusIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <h1>COnteudo Geral</h1>
+      <div className="h-screen flex flex-col items-center pt-4">
+        <div className="flex w-[70%] items-center space-x-2">
+          <Input type="text" placeholder="Produto" className="text-lg"/>
+          <Input type="number" placeholder="Preço (Opcional)" className="w-[30%] text-lg"/>
+          <Button type="submit" variant="default" size="sm" className="rounded-full">
+            <PlusIcon/>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
