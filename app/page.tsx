236b06a6417b59a252b5b8dcd86dc300593735aa
Victor Emanoel <victoremaanoel@outlook.com>
2024-09-28 16:27:05 -0300

    feat: redirect to login on click logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import LoginButton from "@/components/login-button"
 import { ThemeToggle } from "@/components/theme-toggle"
-import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -14,22 +14,34 @@ export default function Home(){
                     <CardTitle className="flex justify-center">Login</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <form>
-                      <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                          <Label htmlFor="name" className="text-lg">Nome:</Label>
-                          <Input id="name" placeholder="Seu nome" className="text-lg"/>
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                          <Label htmlFor="name" className="text-lg">Senha:</Label>
-                          <Input id="password" placeholder="Senha" className="text-lg" />
-                        </div>
+                  <form className="text-left ">
+                    <div className="space-y-6">
+                      <div className="grid w-full items-center gap-1.5">
+                        <Label htmlFor="name" className="text-lg">Nome</Label>
+                        <Input
+                        className="text-base"
+                          name="name"
+                          type="name"
+                          id="name"
+                          placeholder="Seu nome"
+                        />
                       </div>
-                    </form>
+                      <div className="grid w-full items-center gap-1.5">
+                        <Label htmlFor="password" className="text-lg">Senha</Label>
+                        <Input
+                        className="text-base"
+                          name="password"
+                          type="password"
+                          id="password"
+                          placeholder="********"
+                        />
+                      </div>
+                    </div>
+                  </form>
                 </CardContent>
                 <CardFooter className="flex flex-col gap-2">
-                    <Button className="w-full text-base" >Logar</Button>
-                    <Link href="/signIn">
+                    <LoginButton label="Login"/>
+                    <Link href="/register" className="text-base">
                         Cadrastar-se
                     </Link>
                 </CardFooter>
