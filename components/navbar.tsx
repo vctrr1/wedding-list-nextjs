2b646fa30f23b22649497df1a943e2594e86d2a5
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-01 17:51:53 -0300

    feat: make h1 padding left in small devices

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     return (
     <div className="flex flex-row border-b items-center justify-between p-3">
         <div className="hidden sm:flex w-[88px]"></div>
-        <h1 className="text-2xl">Lista de Enxoval</h1>
+        <h1 className="text-2xl pl-3 sm:pl-0">Lista de Enxoval</h1>
         <div className="gap-2 flex">
           <ThemeToggle/>     
           <Button size="icon" variant="ghost" onClick={() => logout()}>
