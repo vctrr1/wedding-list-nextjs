113470ffb9cb3a20d42b5712fafe186c9528c71c
Victor Emanoel <victoremaanoel@outlook.com>
2024-09-30 21:08:02 -0300

    feat: create server action to mark as purschased and delete item

--- actions/items.ts
+++ actions/items.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { db } from "@/lib/db"
+import { revalidatePath } from "next/cache"
 
 export async function createItem(productName: string,productPrice: number | null, productCategory: string, userId: string) {
     await db.item.create({
@@ -11,8 +12,44 @@ export async function createItem(productName: string,productPrice: number | null
             userId: userId
         }
     })
+
+    revalidatePath("/main")
 }
 
-export async function getItemsByCategory() {
-    
+export async function deleteItem(itemId: string){
+    await db.item.delete({
+        where: {
+            id: itemId
+        }
+    })
+
+    revalidatePath("/main")
+}
+
+export async function updateItemAsCompleted(itemId:string, isCompleted: boolean) {
+    try {
+        const item = await db.item.update({
+            where: {
+                id: itemId
+            },
+            data: {
+                purchased: isCompleted
+            }
+
+        })
+        console.log(item)
+    } catch (error) {
+        console.log(error)
+    }
+
+    revalidatePath("/main")
+}
+
+export async function getItemsByCategory(userId: string) {
+    const items = await db.item.findMany({
+        where:{
+            userId: userId
+        }
+    })
+    return items
 }
