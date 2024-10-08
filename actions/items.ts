0762fe166c3e4ef73e57f9884b0893b745dc1666
Victor Emanoel <victoremaanoel@outlook.com>
2024-10-08 01:22:11 -0300

    feat: created server action to create link on item

--- actions/items.ts
+++ actions/items.ts
@@ -77,6 +77,42 @@ export async function updateItem(formData: FormData, category: string) {
 
 }
 
+export async function addLinkToitem(itemId: string, linkUrl: string) {
+    if (!linkUrl) {
+        return { success: false, message: "Erro ao atualizar o item." };
+    }
+
+    // Obter o item atual para pegar os links existentes
+    const item = await db.item.findUnique({
+        where: {
+            id: itemId
+        },
+        select: {
+            links: true
+        }
+    });
+
+    // Se o item existir, atualizar os links
+    if (item) {
+        //se links for um array copia todos os item e coloca o novo link no final, se nao for, cria um novo array contendo apenas o link recebido
+        const updatedLinks = Array.isArray(item.links) ? [...item.links, linkUrl] : [linkUrl];
+
+        await db.item.update({
+            where: {
+                id: itemId
+            },
+            data: {
+                links: updatedLinks
+            }
+        });
+        revalidatePath("/main")
+        return { success: true, message: "Link adicionado com sucesso!" };
+    }
+
+    return { success: false, message: "Item não encontrado." };
+}
+
+
 export async function getItems(userId: string) {
     const items = await db.item.findMany({
         where:{
