06682d2266da1ea1364efb240cb35e43a077c04c
vctrr1 <victoremaanoel@outlook.com>
2025-08-18 03:48:34 -0300

    feat: add new item category

--- app/_constants/categories.ts
+++ app/_constants/categories.ts
@@ -1,36 +1,39 @@
 interface ProductCategories {
-    name: string
-    color: string
-  }
-  
-  export const productCategories: ProductCategories[] = [
-    {
-      name: "Móveis",
-      color: "SlateGray",
-    },
-    {
-      name: "Eletrodomésticos",
-      color: "blueviolet",
-    },
-    {
-      name: "Cozinha",
-      color: "MediumSeaGreen",
-    },
-    {
-      name: "Banheiro",
-      color: "RoyalBlue",
-    },
-    {
-      name: "Lavanderia",
-      color: "CadetBlue",
-    },
-    {
-      name: "Quarto casal",
-      color: "Crimson",
-    },
-    {
-      name: "Sala de estar",
-      color: "Coral",
-    },
-  ]
-  
+  name: string;
+  color: string;
+}
+
+export const productCategories: ProductCategories[] = [
+  {
+    name: "Móveis",
+    color: "SlateGray",
+  },
+  {
+    name: "Eletrodomésticos",
+    color: "blueviolet",
+  },
+  {
+    name: "Cozinha",
+    color: "MediumSeaGreen",
+  },
+  {
+    name: "Banheiro",
+    color: "RoyalBlue",
+  },
+  {
+    name: "Lavanderia",
+    color: "CadetBlue",
+  },
+  {
+    name: "Quarto casal",
+    color: "Crimson",
+  },
+  {
+    name: "Sala de estar",
+    color: "Coral",
+  },
+  {
+    name: "Smart Home",
+    color: "DarkOliveGreen",
+  },
+];
