ad8f2ee76a35d24b81fddaeee150d818c0b08ed5
vctrr1 <victoremaanoel@outlook.com>
2025-08-18 01:26:30 -0300

    chore: update nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import {Inconsolata} from "next/font/google";
+import { Inconsolata } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 
-const inconsolata = Inconsolata({subsets: ['latin'], weight: ["300"]})
+const inconsolata = Inconsolata({ subsets: ["latin"], weight: ["300"] });
 
 export const metadata: Metadata = {
   title: "Wedding List",
@@ -18,20 +18,15 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   
-      <html lang="en" suppressHydrationWarning={true}>
-        <body className={inconsolata.className}>
-          <SessionProvider>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-              >
-              {children}
-              <Toaster/>
-            </ThemeProvider>
-          </SessionProvider>
-        </body>
-      </html>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={inconsolata.className}>
+        <SessionProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
