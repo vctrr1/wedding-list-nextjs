c3dc7c49b46550af1238193f5dc1814afc4957ea
vctrr1 <victoremaanoel@outlook.com>
2025-08-18 17:43:23 -0300

    feat: meke login and register button animated spin while is pending

--- app/_components/login-button.tsx
+++ app/_components/login-button.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
 import { useFormStatus } from "react-dom";
 import React from "react";
+import { Loader2Icon } from "lucide-react";
 
 interface LoginButtonProps {
-    label: string;
-  }
+  label: string;
+}
 
-export default function LoginButton({label}: LoginButtonProps) {
-    const { pending } = useFormStatus();
-    return(
-        <button
-            disabled={pending}
-            type="submit"
-            className={`${
-              pending ? "bg-gray-600" : "bg-blue-600"
-            } rounded-md w-full py-2 text-base font-medium text-white`}
-        >
-        {pending ? "Carregando.." : label}
-        </button>
-
-    )
+export default function LoginButton({ label }: LoginButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      type="submit"
+      className={
+        "flex items-center justify-center rounded-md w-full py-2 text-base font-medium text-white bg-blue-600"
+      }
+    >
+      {pending ? <Loader2Icon className="animate-spin" /> : label}
+    </button>
+  );
 }
