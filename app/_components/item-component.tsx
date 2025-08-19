77718caa8e4ad9e701fafe18b8de055716b2e9d1
vctrr1 <victoremaanoel@outlook.com>
2025-08-18 23:28:33 -0300

    fix: unused variables

--- app/_components/item-component.tsx
+++ app/_components/item-component.tsx
@@ -55,7 +55,7 @@ export default function ItemComponent({
       } else {
         toast.error(result.message);
       }
-    } catch (_) {
+    } catch {
       toast.error("Erro inesperado ao remover item");
     }
   };
@@ -68,7 +68,7 @@ export default function ItemComponent({
       } else {
         toast.error(result.message);
       }
-    } catch (_) {
+    } catch {
       toast.error("Erro inesperado ao remover link");
     }
   };
